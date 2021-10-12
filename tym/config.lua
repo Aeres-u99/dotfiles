3131054bcd2f6be1d1e61acd11ca931b8b4412ca
Akuma <aeres99@hotmail.com>
2021-10-12 15:43:34 +0000

    update

---
+++ tym/config.lua
@@ -0,0 +1,6 @@
+tym.set('font', 'FuraCode Nerd Font 8')
+tym.set_config({
+  shell = '/usr/bin/zsh',
+  cursor_shape = 'ibeam',
+  autohide = false,
+})
---
+++ tym/theme.lua
@@ -0,0 +1,40 @@
+-- hybrid
+local bg = '#1f1f1f'
+local fg = '#fce8c3'
+local cursor = '#fbb829'
+local cursor_fg = '#1c1b19'
+return {
+  color_foreground        = fg,
+  color_background        = bg,
+  color_cursor            = cursor,
+  color_cursor_foreground = cursor_fg,
+    color_0 = "#1c1b19",
+    color_1 = "#918175",
+    color_2 = "#ef2f27",
+    color_3 = "#f75341",
+    color_4 = "#519f50",
+    color_5 = "#98bc37",
+    color_6 = "#fbb829",
+    color_7 = "#fed06e",
+    color_8 = "#2c78bf",
+    color_9 = "#68a8e4",
+    color_10 = "#e02c6d",
+    color_11 = "#ff5c8f",
+    color_12 = "#0aaeb3",
+    color_13 = "#2be4d0",
+    color_14 = "#baa67f",
+    color_15 = "#fce8c3",
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
