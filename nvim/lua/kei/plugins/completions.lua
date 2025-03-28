bc5e7679868dbd91d530708cf15e899753240be2
KeiTachikawa <aeres99@hotmail.com>
2025-03-29 00:20:43 +0530

    Using tab in normal mode for bufferline + Tab in edit mode to cycle through suggestions

--- nvim/lua/kei/plugins/completions.lua
+++ nvim/lua/kei/plugins/completions.lua
@@ -13,12 +13,27 @@ return {
         mapping = cmp.mapping.preset.insert({
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
+          ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }), -- Works in Insert & Select mode
+
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+            }),
+            sources = cmp.config.sources({
+              { name = "nvim_lsp" },
+              { name = "buffer" },
+              { name = "path" },
+            }),
       })
     end,
   },
