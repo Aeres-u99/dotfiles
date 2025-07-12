7b96a6f6a067862cfa313a8eaa634a4c5781c063
KeiTachikawa <aeres99@hotmail.com>
2025-07-12 16:09:53 +0530

    Added new configs

--- nvim/lua/kei/core/keymaps.lua
+++ nvim/lua/kei/core/keymaps.lua
@@ -23,4 +23,30 @@ vim.keymap.set('n', '<C-Space>', function()
 end, { expr = true, noremap = true, silent = true })
 -- Insert a heavy check mark
 vim.api.nvim_set_keymap('i', '<C-k>', '<C-r>=nr2char(0x2714)<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-c>', '"+yy', { noremap = true, silent = true, desc = 'Copy line to clipboard' })
+-- Map Ctrl+C in visual mode to yank to system clipboard
+vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true, desc = 'Copy to clipboard (visual)' })
+
+
+vim.keymap.set('n', '<leader>ve', function()
+  local ve = vim.opt.virtualedit:get()
+  if vim.tbl_contains(ve, 'all') then
+    vim.opt.virtualedit = ''
+    print('virtualedit OFF')
+  else
+    vim.opt.virtualedit = 'all'
+    print('virtualedit ON')
+  end
+end, { desc = 'Toggle virtualedit' })
+
+-- Reload your config
+vim.keymap.set('n', '<leader>rr', function()
+  for name,_ in pairs(package.loaded) do
+    if name:match('^user') then
+      package.loaded[name] = nil
+    end
+  end
+  dofile(vim.env.MYVIMRC)
+  print('🔁 Config reloaded!')
+end, { desc = 'Reload nvim config' })
 
